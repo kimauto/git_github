7473a6cc2ae05d89866e44d788badda88a7ceb04
kimauto <dkskdksk111@gmail.com>
2025-02-07 08:59:16 +0900

    title1
     title의 수정사항은 ...입니다

--- 1.basic_syntax/2.env_setting.sh
+++ 1.basic_syntax/2.env_setting.sh
@@ -1,20 +1,25 @@
-# github인증방법2가지
-# 1.oauth인증방식(제3자인증-웹로그인)
-# 2.pat token인증방식 : github에서 직접 보안키 발급
-# 발급한 키를 키체인(자격증명)에 등록
+ github인증방법2가지
+ 1.oauth인증방식(제3자인증-웹로그인)
+ 2.pat token인증방식 : github에서 직접 보안키 발급
+ 발급한 키를 키체인(자격증명)에 등록
 
-# git 프로젝트 생성방법 2가지
-# 방법1. github에서 진행중인 원격 repo를 clone
-# 방법2. 로컬에서부터 이미 진행중인 프로젝트를 github에 업로드
-# 해당 프로젝트 위치에 .git폴더(repo주소, 사용자 정보 등)가 생성
+git 프로젝트 생성방법 2가지
+방법1. github에서 진행중인 원격 repo를 clone
+방법2. 로컬에서부터 이미 진행중인 프로젝트를 github에 업로드
+해당 프로젝트 위치에 .git폴더(repo주소, 사용자 정보 등)가 생성
+
 git init
-# 원격지 주소를 생성 및 추가
+원격지 주소를 생성 및 추가
+
 git remote add origin 레포주소
-# 원격지 주소 삭제
+원격지 주소 삭제
+
 git remote remove origin
-# 원격지 주소 변경
+원격지 주소 변경
+
 git remote set-url origin 레포주소
-# git 설정정보 조회
+
+git 설정정보 조회
 git config --list
 
 # 타인레포 clone받는 방법 2가지
@@ -49,4 +54,5 @@ git config --list
 
 # 특정 파일을 git 추적목록에서 제외시키고 싶다면 .gitignore파일에 파일목록 등록
 # 이미 add, commit 되어 버린 파일을 추적목록에서 제외하고 싶다면
+# 만약 예를 들어 aws 키값 같은 것을 이미 깃에 올린 경우라면 키값을 변경해야함 
 git rm -r --cached .
